03f8dc9d5deb96e38316b36a53a2e0846977e765
Camden Jenkins <camdenjenkins@yahoo.com>
2022-11-28 13:52:57 -0800

    Orders model possibly finished

---
+++ client/src/store/models/orders.js
@@ -0,0 +1,26 @@
+import { action, thunk } from "easy-peasy";
+import axios from "axios";
+
+export const orders = {
+  data: [],
+  selectedOrder: {},
+  setOrders: action((state, payload) => {
+    state.data = payload;
+  }),
+  fetchOrders: thunk(async (actions, payload) => {
+    const { data } = await axios.get("/routes/orders");
+    actions.setOrders(data);
+  }),
+  addOrder: action((state, payload) => [state.data.push(payload)]),
+  createOrder: thunk(async (actions, payload) => {
+    const { data } = await axios.post("/routes/orders", payload);
+    actions.addOrder(data);
+  }),
+  selectOrder: action((state, payload) => {
+    state.selectedOrder = payload;
+  }),
+  fetchOrder: thunk(async (actions, payload) => {
+    const { data } = await axios.get(`/routes/orders/${payload}`);
+    actions.selectOrder(data);
+  }),
+};
