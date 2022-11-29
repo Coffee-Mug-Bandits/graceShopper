fb8bfa16419a34dcd4fa594e484591f2321f67fe
Camden Jenkins <camdenjenkins@yahoo.com>
2022-11-29 12:28:04 -0800

    products component visible

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import React from "react";
+import Products from "./components/Products";
+// import SingleProduct from "./components/SingleProduct";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Products />
     </div>
   );
 }
---
+++ client/src/components/SingleProduct.jsx
@@ -0,0 +1,12 @@
+// import { useEffect } from "react";
+// import useProducts from "../hooks/useProducts";
+
+// export default function SingleProduct() {
+//   const { selectedProduct, fetchProduct } = useProducts();
+
+//   useEffect(() => {
+//     fetchProduct();
+//   }, []);
+
+//   return <div>{JSON.stringify(selectedProduct)}</div>;
+// }
