348c2f8095f07dc90ef2f0a9f8969bd5a93d17dc
Camden Jenkins <camdenjenkins@yahoo.com>
2022-11-30 08:50:26 -0800

    little fix

--- routes/user.js
+++ routes/user.js
@@ -1,111 +1,109 @@
 const userRouter = require("express").Router();
-const prisma = require("../prisma/prisma")
-const { asyncErrorHandler, authRequired } = require("./utils")
+const prisma = require("../prisma/prisma");
+const { asyncErrorHandler, authRequired } = require("./utils");
 const bcrypt = require("bcrypt");
 const SALT_ROUNDS = 10;
-const { JWT_SECRET } = process.env; 
+const { JWT_SECRET } = process.env;
 const jwt = require("jsonwebtoken");
 const { Users } = require("../prisma/seedData");
 
-userRouter.get (
-    "/",
-    asyncErrorHandler(async (req, res, next) => {
-const users = await prisma.Users.findMany();
-res.send(users);
-})
+userRouter.get(
+  "/",
+  asyncErrorHandler(async (req, res, next) => {
+    const users = await prisma.Users.findMany();
+    res.send(users);
+  })
 );
 
-userRouter.post (
-    "/register", 
-    asyncErrorHandler(async (req, res, next) => {
-        const { username, password, email, location } = req.body; 
-        const checkuser = await prisma.Users.findUnique({
-            where: {
-                username: username, 
-            }
-        })
-        if (checkuser) {
-            next({
-                name: "NotNewUserError",
-                message: "That username has been taken",
-              });
-        }
-
-        const hashedpassword = await bcrypt.hash(password, SALT_ROUNDS);
-        const user = await prisma.Users.create({
-            data: {
-                username: username,
-                password: hashedpassword,
-                email: email, 
-                location: location
-            }
-        })
-        delete user.password;
-        const token = jwt.sign(user, JWT_SECRET);
-
-        res.cookie("token", token, {
-            sameSite: "strict",
-            httpOnly: true,
-            signed: true,
-          });
-
-
-        res.send(user)
-    })
-
-)
-
-userRouter.post (
-    "/login",
-    asyncErrorHandler(async (req, res, next) => {
-        const { username, password } = req.body;
-        const user = await prisma.Users.findUnique({
-            where: {
-                username
-            }
-        });
-        const validpassword = await bcrypt.compare(password, user.password);
-        if (validpassword) {
-            const token = jwt.sign(user, JWT_SECRET);
-
-            res.cookie("token", token, {
-                sameSite: "strict",
-                httpOnly: true,
-                signed: true,
-            })
-
-        delete Users.password;
-        res.send({ user })
-    } else next({
-        name: "Invaild Login",
-        message: "Username or Password is incorrect",
+userRouter.post(
+  "/register",
+  asyncErrorHandler(async (req, res, next) => {
+    const { username, password, email, location } = req.body;
+    const checkuser = await prisma.Users.findUnique({
+      where: {
+        username: username,
+      },
+    });
+    if (checkuser) {
+      next({
+        name: "NotNewUserError",
+        message: "That username has been taken",
       });
-    })
+    }
+
+    const hashedpassword = await bcrypt.hash(password, SALT_ROUNDS);
+    const user = await prisma.Users.create({
+      data: {
+        username: username,
+        password: hashedpassword,
+        email: email,
+        location: location,
+      },
+    });
+    delete user.password;
+    const token = jwt.sign(user, JWT_SECRET);
+
+    res.cookie("token", token, {
+      sameSite: "strict",
+      httpOnly: true,
+      signed: true,
+    });
+
+    res.send(user);
+  })
 );
 
-userRouter.get("/me", authRequired, async (req, res, next) => {
-    try {
-      res.send(req.user);
-    } catch (error) {
-      next(error);
-    }
-  });
-  
+userRouter.post(
+  "/login",
+  asyncErrorHandler(async (req, res, next) => {
+    const { username, password } = req.body;
+    const user = await prisma.Users.findUnique({
+      where: {
+        username,
+      },
+    });
+    const validpassword = await bcrypt.compare(password, user.password);
+    if (validpassword) {
+      const token = jwt.sign(user, JWT_SECRET);
 
-  userRouter.post("/logout", async (req, res, next) => {
-    try {
-      res.clearCookie("token", {
+      res.cookie("token", token, {
         sameSite: "strict",
         httpOnly: true,
         signed: true,
       });
-      res.send({
-        loggedIn: false,
-        message: "Logged Out",
+
+      delete Users.password;
+      res.send(user);
+    } else
+      next({
+        name: "Invaild Login",
+        message: "Username or Password is incorrect",
       });
-    } catch (error) {
-      next(error);
-    }
-  });
-  
-module.exports = userRouter
+  })
+);
+
+userRouter.get("/me", authRequired, async (req, res, next) => {
+  try {
+    res.send(req.user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.post("/logout", async (req, res, next) => {
+  try {
+    res.clearCookie("token", {
+      sameSite: "strict",
+      httpOnly: true,
+      signed: true,
+    });
+    res.send({
+      loggedIn: false,
+      message: "Logged Out",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = userRouter;
