51828af80165ea43f5c7f5be9c3dbd9e79e97c25
AgoraphobicClimber <wyattbagnall03@gmail.com>
2022-12-01 13:55:35 -0800

    updatedqty

---
+++ client/src/hooks/useCart.js
@@ -0,0 +1,32 @@
+import { useStoreState, useStoreActions } from "easy-peasy";
+
+export default function useCart() {
+
+    const cart = useStoreState((state) => {
+      return state.cart.cart;
+    });
+
+    const fetchCart = useStoreActions((actions) => {
+        console.log(actions);
+        return actions.cart.fetchCart;
+      });
+
+    const updateQty = useStoreActions((actions) => {
+        console.log(actions);
+        return actions.cart.editQty;
+    });
+
+    const deleteItem = useStoreActions((actions) => {
+        console.log(actions);
+        return actions.cart.deleteItem; 
+    });
+    
+    return (
+        cart,
+        fetchCart,
+        updateQty,
+        deleteItem
+    )
+
+}
+
