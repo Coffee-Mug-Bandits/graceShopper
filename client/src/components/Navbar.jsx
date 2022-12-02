1cfa1acd2eddf12b189f8f898139a8215e598150
angie <a.hinkson5@gmail.com>
2022-12-02 12:52:29 -0800

    nav changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,53 +3,39 @@ import { useNavigate } from "react-router-dom";
 import useUsers from "../hooks/useUsers";
 
 export function Navbar() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const {selectedUser, logoutUser} = useUsers();
-return(
+  const { selectedUser, logoutUser } = useUsers();
+  return (
     <div>
-    <Link to="/products">
-    Shop
-    </Link>
-    <form>
-    <input
-    placeholder="Search"
-    >
-    </input>
-    </form>
-    {!selectedUser ? (
-    <>
-    <Link to="/register">
-    Register
-    </Link>
-
-    <Link to="/login">
-    Login
-    </Link>
-
-    </>
-    ): null}
-
-
-    {selectedUser ? (
+      {console.log(selectedUser)}
+      <Link to="/products">Shop</Link>
+      <form>
+        <input placeholder="Search"></input>
+      </form>
+      {selectedUser.username === "Guest" ? (
         <>
-         <Link to="/cart">
-        My Cart
-        </Link>
+          <Link to="/register">Register</Link>
 
-        <Link to="/profile">
-        My Account
-        </Link>
-        <button
-              onClick={() => {
-                logoutUser();
-                navigate("/products");
-              }}
-            >
-              Logout
-            </button>
+          <Link to="/login">Login</Link>
         </>
-    ): null}
+      ) : null}
+
+      {selectedUser.username !== "Guest" ? (
+        <>
+          <Link to="/cart">My Cart</Link>
+
+          <Link to="/profile">My Account</Link>
+          <button
+            onClick={() => {
+              logoutUser();
+              navigate("/products");
+            }}
+          >
+            Logout
+          </button>
+        </>
+      ) : null}
     </div>
-);
+  );
 }
