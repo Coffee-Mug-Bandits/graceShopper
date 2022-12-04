1d1b677e282f71003e0a2ce6bd0f76ea39ac7164
Aundrina <aundrinaleyva@yahoo.com>
2022-12-03 20:20:17 -0800

    tw

--- client/src/components/Cards/CartCard.jsx
+++ client/src/components/Cards/CartCard.jsx
@@ -1,19 +1,15 @@
-import productCard from "./ProductsCard"
-
-export default function CartCard({cart}) {
-    return (
-       <div>{cart.order_products.map((op) => {
-            return (
-                <h2>
-                    {op.products.name}
-                <h2>
-                    {op.products.price}
-                </h2>
-                <h2>
-                    {op.qty}
-                </h2>
-                </h2>
-            )
-       })}</div>
-    )
+export default function CartCard({ cart }) {
+  return (
+    <div>
+      {cart.order_products.map((op) => {
+        return (
+          <h2>
+            {op.products.name}
+            <h2>{op.products.price}</h2>
+            <h2>{op.qty}</h2>
+          </h2>
+        );
+      })}
+    </div>
+  );
 }
