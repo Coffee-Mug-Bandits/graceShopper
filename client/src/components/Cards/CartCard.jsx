9d13e255eb4aebd2002127058529eac403a990e6
AgoraphobicClimber <wyattbagnall03@gmail.com>
2022-12-03 19:24:10 -0800

    cartcard

---
+++ client/src/components/Cards/CartCard.jsx
@@ -0,0 +1,19 @@
+import productCard from "./ProductsCard"
+
+export default function CartCard({cart}) {
+    return (
+       <div>{cart.order_products.map((op) => {
+            return (
+                <h2>
+                    {op.products.name}
+                <h2>
+                    {op.products.price}
+                </h2>
+                <h2>
+                    {op.qty}
+                </h2>
+                </h2>
+            )
+       })}</div>
+    )
+}
