f7bbfaa0abfee822159efc3ef171e15a3a3f86f6
AgoraphobicClimber <wyattbagnall03@gmail.com>
2022-12-05 10:45:44 -0800

    single card

--- client/src/components/Cards/SingleProductCard.jsx
+++ client/src/components/Cards/SingleProductCard.jsx
@@ -1,9 +1,19 @@
 export default function SingleProductCard({ selectedProduct }) {
   return (
-    <div>
-      <h2>{selectedProduct.name}</h2>
-      <h3>{selectedProduct.description}</h3>
-      <h3>{selectedProduct.price}</h3>
+  <div className="flex items-center flex-col justify-center mt-44">
+     <h2 className="text-5xl mb-2">{selectedProduct.name}</h2>
+    <div className="flex justify-center bg-gradient-to-t from-yellow-900 to-yellow-600 items-center flex-row flex-wrap border-black border-x-2 border-y-2 h-96 w-screen rounded-md text-yellow-50">
+    <img
+        className="flex justify-center h-80 mt-4"
+        src="https://m.media-amazon.com/images/I/717zDSCMKPL._AC_SX425_.jpg"
+      />
+      <h3 className="text-2xl absolute right-28 mb-36">Price: {selectedProduct.price}</h3>
+      <button className="text-2xl absolute right-28 mt-20 bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Add to Cart </button>
+      <button className="text-2xl absolute right-28 mb-12 bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Back to Shop</button>
     </div>
+    <h3 className="text-3xl">{selectedProduct.description}</h3>
+  </div>
   );
 }
+
+// https://www.officedepot.com/a/products/4582120/Shaquille-ONeal-Zephyrus-Ergonomic-Bonded-Leather/
