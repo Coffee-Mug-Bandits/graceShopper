21e64f6b3737fe4d26b49391118c5c9d50ee05c6
AgoraphobicClimber <wyattbagnall03@gmail.com>
2022-12-06 13:05:04 -0800

    new products

--- prisma/seedData.js
+++ prisma/seedData.js
@@ -60,7 +60,7 @@ const Product = [
   {
     seller_id: 1,
     name: "Red Coffee Mug",
-    price: 600,
+    price: 7,
     stock_qty: 7,
     description: "A red mug to drink from in the morning",
     rating: 4,
@@ -68,7 +68,7 @@ const Product = [
   {
     seller_id: 2,
     name: "Colombian Coffee Grounds",
-    price: 1300,
+    price: 13,
     stock_qty: 57,
     description: "delicious coffee from Colombia",
     rating: 5,
@@ -76,9 +76,117 @@ const Product = [
   {
     seller_id: 3,
     name: "Coffee Pot",
-    price: 7000,
+    price: 7,
     stock_qty: 12,
     description: "Brew your coffee bright and early",
+
+    rating: 3,
+  },
+  {
+    seller_id: 2,
+    name: "Coffee Mug 4oz",
+    price: 8,
+    stock_qty: 20,
+    description: "You a mug expert? Didn't think so, so buy this one",
+
+    rating: 4,
+  },
+  {
+    seller_id: 3,
+    name: "Coffee Mug 8oz",
+    price: 11,
+    stock_qty: 12,
+    description: "Like the one earlier but bigger and better",
+
+    rating: 3,
+  },
+  {
+    seller_id: 1,
+    name: "Coffee Mug 14oz",
+    price: 15,
+    stock_qty: 17,
+    description: "That 8oz mug? Pathetic, this one is better, don't listen to that last guy",
+
+    rating: 1,
+  },
+  {
+    seller_id: 4,
+    name: "Travel Tumbler",
+    price: 13,
+    stock_qty: 12,
+    description: "For busy people who aren't able to spend quality time with their coffee or children",
+
+    rating: 3,
+  },
+  {
+    seller_id: 1,
+    name: "Espresso Machine",
+    price: 150,
+    stock_qty: 12,
+    description: "Unless you're an addict this is a terrible investment",
+
+    rating: 4,
+  },
+  {
+    seller_id: 3,
+    name: "Coffee Filters",
+    price: 10,
+    stock_qty: 12,
+    description: "How many are in the package? Roll a d20 to find out",
+
+    rating: 3,
+  },
+  {
+    seller_id: 4,
+    name: "Spoons",
+    price: 3,
+    stock_qty: 11,
+    description: "Imagine selling any other utensil",
+
+    rating: 3,
+  },
+  {
+    seller_id: 1,
+    name: "Warming Plate",
+    price: 26,
+    stock_qty: 6,
+    description: "Doubles as a tattoo remover",
+
+    rating: 3,
+  },
+  {
+    seller_id: 4,
+    name: "Sugar",
+    price: 4,
+    stock_qty: 12,
+    description: "I'm not gonna sugarcoat it, this is overpriced",
+
+    rating: 3,
+  },
+  {
+    seller_id: 4,
+    name: "Powdered Creamer",
+    price: 5,
+    stock_qty: 12,
+    description: "Can't think of a quip for this one, buy it anyways",
+
+    rating: 3,
+  },
+  {
+    seller_id: 3,
+    name: "Liquid Flavored Creamer",
+    price: 7,
+    stock_qty: 12,
+    description: "Just drink straight out the container, I don't judge",
+
+    rating: 3,
+  },
+  {
+    seller_id: 2,
+    name: "Wall Mounted Mug Rack",
+    price: 30,
+    stock_qty: 12,
+    description: "To display our amazing products!",
 
     rating: 3,
   },
